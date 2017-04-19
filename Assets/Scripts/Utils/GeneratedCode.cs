1a8babe7b3b4f3e9c130f11a4f90a3154cafbdf7
Dante Camarena <mhdcj@hotmail.com>
2017-04-19 08:16:44 -0400

    Codegen and prefabs

--- Assets/Scripts/Utils/GeneratedCode.cs
+++ Assets/Scripts/Utils/GeneratedCode.cs
@@ -46,10 +46,12 @@ public static IList Create(Type t) => dict[t]();
 
 static OrbLists()
 {
+dict[typeof(Gravity)] = ()=> new List< Gravity>();
 dict[typeof(Core)] = ()=> new List< Core>();
 dict[typeof(ForceMove)] = ()=> new List< ForceMove>();
-dict[typeof(Tracer)] = ()=> new List< Tracer>();
-dict[typeof(Bomb)] = ()=> new List< Bomb>();
+dict[typeof(Shooter)] = ()=> new List< Shooter>();
+dict[typeof(HueShifter)] = ()=> new List< HueShifter>();
+dict[typeof(Orb)] = ()=> new List< Orb>();
 dict[typeof(IDrawOrb)] = ()=> new List< IDrawOrb>();
 dict[typeof(IAffectSelfOrb)] = ()=> new List< IAffectSelfOrb>();
 dict[typeof(IAffectOtherOrb)] = ()=> new List< IAffectOtherOrb>();
@@ -58,12 +60,11 @@ dict[typeof(IFixedAffectSelf)] = ()=> new List< IFixedAffectSelf>();
 dict[typeof(IAimedActionOrb)] = ()=> new List< IAimedActionOrb>();
 dict[typeof(IActionOrb)] = ()=> new List< IActionOrb>();
 dict[typeof(IMovementOrb)] = ()=> new List< IMovementOrb>();
-dict[typeof(Lifetime)] = ()=> new List< Lifetime>();
-dict[typeof(HueShifter)] = ()=> new List< HueShifter>();
-dict[typeof(Gravity)] = ()=> new List< Gravity>();
+dict[typeof(Bomb)] = ()=> new List< Bomb>();
 dict[typeof(Player)] = ()=> new List< Player>();
-dict[typeof(Shooter)] = ()=> new List< Shooter>();
-dict[typeof(Orb)] = ()=> new List< Orb>();
+dict[typeof(Lifetime)] = ()=> new List< Lifetime>();
+dict[typeof(Watch)] = ()=> new List< Watch>();
+dict[typeof(Tracer)] = ()=> new List< Tracer>();
 
 }
 }
